ae46883f954af317c563361d41f258d523931725
juanxo13597 <juanxo97master@gmail.com>
2024-03-27 16:57:18 +0100

    test: :zap: initial config test

---
+++ src/App.test.tsx
@@ -0,0 +1,6 @@
+import { describe, it, expect } from "vitest";
+describe("app", () => {
+  it("render", () => {
+    expect(true).toBe(true);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    coverage: {
+      provider: "v8",
+      enabled: true,
+      include: ["**/src/lib/**"],
+      exclude: ["**/index.ts"],
+      thresholds: {
+        statements: 100,
+        branches: 100,
+        functions: 100,
+        lines: 100,
+      },
+    },
+    setupFiles: "./setup-test.ts",
+  },
+});
