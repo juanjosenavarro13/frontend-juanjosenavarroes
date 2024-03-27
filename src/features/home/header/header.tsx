ea7c742a22bf3a24f1fb19c92b4d2df0e661f7b4
juanxo13597 <juanxo97master@gmail.com>
2024-03-27 21:24:57 +0100

    test(home): :white_check_mark: add test home, header

--- src/features/home/header/header.tsx
+++ src/features/home/header/header.tsx
@@ -24,7 +24,10 @@ const navItems = [
 ];
 export function Header() {
   return (
-    <header className="fixed top-0 z-10 flex items-center justify-center w-full mx-auto mt-2">
+    <header
+      data-testid="header"
+      className="fixed top-0 z-10 flex items-center justify-center w-full mx-auto mt-2"
+    >
       <nav className="flex px-3 text-sm font-medium rounded-full text-gray-600 dark:text-gray-200 justify-center items-center">
         {navItems.map((link) => (
           <a
--- src/features/home/home-page.test.tsx
+++ src/features/home/home-page.test.tsx
@@ -1,8 +1,12 @@
 import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import HomePage from "./home-page";
 describe("home page", () => {
   it("render", () => {
     render(<HomePage />);
+  });
+  it("show header", () => {
+    const { getByTestId } = render(<HomePage />);
+    expect(getByTestId("header")).toBeInTheDocument();
   });
 });
