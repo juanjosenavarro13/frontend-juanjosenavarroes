15513f8196e6650d63c8d0451e41dabad0a23f94
juanxo13597 <juanxo97master@gmail.com>
2024-03-28 15:49:00 +0100

    feat(home): :sparkles: add about me

--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -5,3 +5,4 @@ export * from "./section-container/section-container";
 export * from "./hero/hero";
 export * from "./experience/experience";
 export * from "./projects/projects";
+export * from "./about-me/about-me";
--- src/features/home/home-page.tsx
+++ src/features/home/home-page.tsx
@@ -1,5 +1,6 @@
 import { useStoreTheme } from "@/stores";
 import {
+  AboutMe,
   Background,
   Experience,
   Footer,
@@ -66,13 +67,28 @@ export default function HomePage() {
             </h2>
             <Projects />
           </SectionContainer>
-          {/* <SectionContainer id="sobre-mi">
+          <SectionContainer id="sobre-mi">
             <h2 className="flex items-center mb-6 text-3xl font-semibold gap-x-8 text-black/80 dark:text-white">
-              <ProfileCheck className="size-8" />
+              <svg
+                className="size-8"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4"></path>
+                <path d="M15 19l2 2l4 -4"></path>
+              </svg>
               Sobre mí
             </h2>
             <AboutMe />
-          </SectionContainer> */}
+          </SectionContainer>
         </div>
       </main>
       <Footer />
