7505fe4ddb10732df3672e97b365640ffd1fc0af
juanxo13597 <juanxo97master@gmail.com>
2024-03-28 19:59:26 +0100

    fix: :art: change button for seo

--- src/features/portfolio/components/hero/components/badge.tsx
+++ src/features/portfolio/components/hero/components/badge.tsx
@@ -2,7 +2,7 @@ export function Badge({
   children,
 }: Readonly<{ children?: JSX.Element | string }>) {
   return (
-    <div className="flex items-center m-3">
+    <div className="flex items-center h-12">
       <span className="relative inline-flex overflow-hidden rounded-full p-[1px]">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#51E4B8_0%,#21554E_50%,#51E4B8_100%)]"></span>
         <div className="inline-flex items-center justify-center w-full px-3 py-1 text-sm text-green-800 bg-green-100 rounded-full cursor-pointer dark:bg-gray-800 dark:text-white/80 backdrop-blur-3xl whitespace-nowrap">
