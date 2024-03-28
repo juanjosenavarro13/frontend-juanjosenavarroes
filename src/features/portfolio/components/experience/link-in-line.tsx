6533b4ea32c6cbc485fe258c6653059baf5713bb
juanxo13597 <juanxo97master@gmail.com>
2024-03-28 19:55:27 +0100

    style: :art: seo

--- src/features/portfolio/components/experience/link-in-line.tsx
+++ src/features/portfolio/components/experience/link-in-line.tsx
@@ -8,7 +8,7 @@ export function LinkInLine({
   return (
     <a
       href={href}
-      className="inline-flex items-center text-lg font-medium text-blue-500 dark:text-blue-200 dark:hover:text-blue-300 hover:text-blue-700"
+      className="h-12 inline-flex items-center text-lg font-medium text-blue-500 dark:text-blue-200 dark:hover:text-blue-300 hover:text-blue-700"
     >
       {children}
     </a>
