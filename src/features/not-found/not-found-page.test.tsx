bcfd371bdb32e59cf5ddcd0e53d90f52964fb47d
juanxo13597 <juanxo97master@gmail.com>
2024-03-28 18:48:19 +0100

    feat(not-found): :art: style not found

--- src/features/not-found/not-found-page.test.tsx
+++ src/features/not-found/not-found-page.test.tsx
@@ -1,13 +1,15 @@
 import { render } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import NotFoundPage from "./not-found-page";
+import { BrowserRouter } from "react-router-dom";
 describe("not found page", () => {
-  it("render", () => {
-    const { getByTestId } = render(<NotFoundPage />);
-    expect(getByTestId("page404")).toBeInTheDocument();
-  });
-  it("404 text", () => {
-    const { getByText } = render(<NotFoundPage />);
+  it("show 404 text", () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <NotFoundPage />
+      </BrowserRouter>,
+    );
     expect(getByText("404")).toBeInTheDocument();
+    expect(getByText("Página principal").getAttribute("href")).toBe("/");
   });
 });
--- src/features/not-found/not-found-page.tsx
+++ src/features/not-found/not-found-page.tsx
@@ -1,3 +1,36 @@
+import { Link } from "react-router-dom";
+
 export default function NotFoundPage() {
-  return <main data-testid="page404">404</main>;
+  return (
+    <div className="flex items-center justify-center min-h-dvh">
+      <div className="text-center">
+        <h1 className="mb-4 text-6xl font-semibold text-blue-500">404</h1>
+        <p className="mb-4 text-lg text-gray-600">
+          Oops! La página que buscas no existe.
+        </p>
+        <div className="animate-bounce">
+          <svg
+            className="mx-auto h-16 w-16 text-blue-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+            ></path>
+          </svg>
+        </div>
+        <p className="mt-4 text-gray-600">
+          Ir a la{" "}
+          <Link to="/" className="text-blue-500">
+            Página principal
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
