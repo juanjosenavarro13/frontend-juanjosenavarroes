dd0839d1d0faf0d26d93becd5bb8b3d9a5210ea7
juanxo13597 <juanxo97master@gmail.com>
2024-03-29 14:46:04 +0100

    feat(portfolio): :memo: add translations hero

---
+++ src/features/portfolio/constants/info.ts
@@ -0,0 +1,11 @@
+const name = "Juan José";
+const firstSurname = "Navarro";
+const secondSurname = "Perea";
+
+export const INFO = {
+  name,
+  firstSurname,
+  secondSurname,
+  fullName: name + " " + firstSurname + " " + secondSurname,
+  linkedin: "https://www.linkedin.com/in/juan-jose-navarro-perea/",
+};
