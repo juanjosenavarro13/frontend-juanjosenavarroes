8bfcd9fa3bab1b8d2a6ffdc98cb178abb20394b4
juanxo13597 <juanxo97master@gmail.com>
2024-03-29 14:59:50 +0100

    feat(portfolio): :memo: add translations experience section

---
+++ src/features/portfolio/icons/arrow-right-icon.tsx
@@ -0,0 +1,19 @@
+export function ArrowRightIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M9 6l6 6l-6 6" />
+    </svg>
+  );
+}
--- src/features/portfolio/sections/experience/components/experience-item.tsx
+++ src/features/portfolio/sections/experience/components/experience-item.tsx
@@ -1,5 +1,7 @@
+import { useTranslation } from "react-i18next";
 import { Experiencie } from "../../../constants/experiencie";
 import { LinkInLine } from "./link-in-line";
+import { ArrowRightIcon } from "@/features/portfolio/icons";
 
 interface ExperienceItemProps {
   experiencieItem: Experiencie;
@@ -7,6 +9,7 @@ interface ExperienceItemProps {
 export function ExperienceItem({
   experiencieItem,
 }: Readonly<ExperienceItemProps>) {
+  const { t } = useTranslation("portfolio");
   const { company, date, description, link, title } = experiencieItem;
   return (
     <div className="relative mx-12 pb-12 grid before:absolute before:left-[-35px] before:block before:h-full before:border-l-2 before:border-black/20 dark:before:border-white/15 before:content-[''] md:grid-cols-5 md:gap-10 md:space-x-4]">
@@ -21,31 +24,17 @@ export function ExperienceItem({
             {company}
           </h4>
           <time className="p-0 m-0 text-sm text-gray-600/80 dark:text-white/80">
-            {date}
+            {t(date)}
           </time>
         </div>
       </div>
       <div className="relative flex flex-col gap-2 pb-4 text-gray-600 dark:text-gray-300 md:col-span-3">
-        {description}
+        {t(description)}
         {link && (
           <LinkInLine href={link}>
             <>
-              Saber más{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-5 icon icon-tabler icon-tabler-chevron-right"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M9 6l6 6l-6 6" />
-              </svg>
+              {t("experienceSection.knowMore")}{" "}
+              <ArrowRightIcon className="w-5" />
             </>
           </LinkInLine>
         )}
