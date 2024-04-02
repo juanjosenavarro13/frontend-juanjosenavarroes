76749af6a7c7e7ae0f80fd43cc25dc1323846aaa
juanxo13597 <juanxo97master@gmail.com>
2024-04-02 16:01:11 +0200

    feat(auth): :art: html and style login

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Aquí se encuentra una lista de tareas pendientes:
 
-- [x] Refactorizar portfolio
-- [x] Instalar i18n backend
+- [ ] Login
+- [ ] Administración
--- src/features/auth/login.tsx
+++ src/features/auth/login.tsx
@@ -1,3 +1,53 @@
 export default function Login() {
-  return <>login</>;
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-200">
+      <div className="w-96 rounded bg-white p-8 shadow-md">
+        <h2 className="mb-4 text-center text-2xl font-bold">Iniciar sesión</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="mb-2 block text-sm font-bold text-gray-700"
+            >
+              Correo electrónico
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="mb-2 block text-sm font-bold text-gray-700"
+            >
+              Contraseña
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+            >
+              Iniciar sesión
+            </button>
+            <a
+              href="#"
+              className="inline-block align-baseline text-sm font-bold text-blue-500 hover:text-blue-800"
+            >
+              ¿Olvidaste tu contraseña?
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
