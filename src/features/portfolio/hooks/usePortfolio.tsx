ecfb4ebb740cc589067670f2b84bb4feafb2a8ff
juanxo13597 <juanxo97master@gmail.com>
2024-04-28 21:20:23 +0200

    fix: :art: fix change language

--- src/features/portfolio/hooks/usePortfolio.tsx
+++ src/features/portfolio/hooks/usePortfolio.tsx
@@ -1,18 +1,19 @@
 import { HTTP_ENDPOINTS } from "@/core/constants/http-endpoints";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useTranslation } from "react-i18next";
 import { PortfolioResponse } from "../types";
+import { useTranslation } from "react-i18next";
 
 export function usePortfolio() {
   const { i18n } = useTranslation();
-  const lang = i18n.language;
-
   const { isPending, isError, data } = useQuery({
-    queryKey: ["portfolio"],
+    queryKey: ["portfolio", i18n.language],
     staleTime: Infinity,
     queryFn: () =>
-      axios.get<PortfolioResponse>(HTTP_ENDPOINTS.portfolio + "?lang=" + lang),
+      axios.get<PortfolioResponse>(
+        HTTP_ENDPOINTS.portfolio + "?lang=" + i18n.language,
+      ),
   });
+
   return { isPending, isError, portfolio: data?.data };
 }
