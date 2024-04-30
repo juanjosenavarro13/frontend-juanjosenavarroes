c7a97d7c6d87d45e8015bc666cd3cecc4d1802ff
juanxo13597 <juanxo97master@gmail.com>
2024-04-30 21:26:40 +0200

    feat: :art: skeleton about me

--- src/features/portfolio/sections/about-me/about-me-skeleton.tsx
+++ src/features/portfolio/sections/about-me/about-me-skeleton.tsx
@@ -2,30 +2,30 @@ import { LoadingImage } from "../../components";
 
 export function AboutMeSkeleton() {
   return (
-    <article className="flex h-[45rem] flex-col sm:h-56 sm:flex-row">
-      <div className="order-2 mt-10 flex w-full animate-pulse flex-col gap-3 sm:pr-10">
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 h-2 rounded bg-gray-400"></div>
-        <div className="col-span-2 block h-2 rounded bg-gray-400 sm:hidden"></div>
-        <div className="col-span-2 block h-2 rounded bg-gray-400 sm:hidden"></div>
-        <div className="col-span-2 block h-2 rounded bg-gray-400 sm:hidden"></div>
-        <div className="col-span-2 block h-2 rounded bg-gray-400 sm:hidden"></div>
-        <div className="col-span-2 block h-2 rounded bg-gray-400 sm:hidden"></div>
-        <div className="col-span-2 block h-2 rounded bg-gray-400 sm:hidden"></div>
-        <div className="col-span-2 block h-2 rounded bg-gray-400 sm:hidden"></div>
+    <article className="flex h-[40rem] flex-col items-center justify-center md:h-[17rem] md:flex-row">
+      <div className="order-2 mt-10 flex w-full animate-pulse flex-col gap-3 sm:pr-10 md:order-1">
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="h-2 rounded bg-gray-400"></div>
+        <div className="block h-2 rounded bg-gray-400 sm:hidden"></div>
+        <div className="block h-2 rounded bg-gray-400 sm:hidden"></div>
+        <div className="block h-2 rounded bg-gray-400 sm:hidden"></div>
+        <div className="block h-2 rounded bg-gray-400 sm:hidden"></div>
+        <div className="block h-2 rounded bg-gray-400 sm:hidden"></div>
+        <div className="block h-2 rounded bg-gray-400 sm:hidden"></div>
+        <div className="block h-2 rounded bg-gray-400 sm:hidden"></div>
       </div>
-      <div className="order-1 text-gray-800 md:order-2 lg:w-64 lg:p-2">
-        <LoadingImage className="rotate-3 rounded-2xl bg-black/20 object-cover p-1 ring-1 ring-black/70 dark:bg-blue-500/5 dark:ring-white/20" />
+      <div className="order-1 text-gray-800 md:order-2">
+        <LoadingImage className="h-[200px] rotate-3 rounded-2xl bg-black/20 object-cover p-1 ring-1 ring-black/70 dark:bg-blue-500/5 dark:ring-white/20" />
       </div>
     </article>
   );
--- src/features/portfolio/sections/about-me/about-me.tsx
+++ src/features/portfolio/sections/about-me/about-me.tsx
@@ -7,7 +7,7 @@ export function AboutMe() {
   if (!portfolio) return <AboutMeSkeleton />;
   const { info } = portfolio;
   return (
-    <article className="flex h-[45rem] flex-col items-center justify-center gap-8 text-gray-700 dark:text-gray-300 sm:h-56 md:flex-row">
+    <article className="flex h-[40rem] flex-col items-center justify-center gap-8 text-gray-700 dark:text-gray-300 md:h-[17rem] md:flex-row">
       <div className="order-2 text-pretty md:order-1 [&>p>strong]:font-mono [&>p>strong]:font-normal [&>p>strong]:text-blue-500 dark:[&>p>strong]:text-blue-100 [&>p]:mb-4">
         <p>
           <Trans
@@ -23,7 +23,7 @@ export function AboutMe() {
         height="200"
         src="/juanjose.avif"
         alt={info.fullName}
-        className="order-1 aspect-square h-full w-64 rotate-3 rounded-2xl bg-black/20 object-cover p-1 ring-1 ring-black/70 dark:bg-blue-500/5 dark:ring-white/20 md:order-2 lg:w-64 lg:p-2"
+        className="order-1 aspect-square rotate-3 rounded-2xl bg-black/20 object-cover p-1 ring-1 ring-black/70 dark:bg-blue-500/5 dark:ring-white/20 md:order-2 lg:w-64 lg:p-2"
         style={{ objectPosition: "50% 50%" }}
         loading="lazy"
       />
