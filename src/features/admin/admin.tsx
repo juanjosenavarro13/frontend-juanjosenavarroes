a528d45e9e1a9fad241106e82e161d579cf891a5
juanxo13597 <juanxo97master@gmail.com>
2024-05-01 19:21:10 +0200

    feat: :zap: delete translates admin module

--- src/features/admin/admin.tsx
+++ src/features/admin/admin.tsx
@@ -1,6 +1,3 @@
-import { useTranslation } from "react-i18next";
-
 export default function Admin() {
-  const { i18n } = useTranslation();
-  return <div data-testid="adminPage">admin {i18n.language}</div>;
+  return <div data-testid="adminPage">admin</div>;
 }
