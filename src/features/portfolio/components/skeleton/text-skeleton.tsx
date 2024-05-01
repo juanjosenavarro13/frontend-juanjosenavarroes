351234692d0881725fca3b13e72a12d6d051711a
juanxo13597 <juanxo97master@gmail.com>
2024-05-01 16:36:30 +0200

    fix: :art: fix skeleton component

--- src/features/portfolio/components/skeleton/text-skeleton.tsx
+++ src/features/portfolio/components/skeleton/text-skeleton.tsx
@@ -1,11 +1,11 @@
 export function TextSkeleton({
-  span,
-  h,
+  span = 1,
+  h = 2,
   className,
 }: Readonly<{ span?: number; h?: number; className?: string }>) {
   return (
     <div
-      className={`col-span-${span ?? 1} h-${h ?? 2} rounded bg-gray-400 ${className} animate-pulse`}
+      className={`col-span-${span} h-${h} rounded bg-gray-400 ${className} animate-pulse`}
     ></div>
   );
 }
