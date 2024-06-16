5b93a6346d91d4c8cce9cda46ac21b9fef53aef8
juan jose navarro perea <juanxo97master@gmail.com>
2024-06-16 17:44:27 +0200

    feat: expose portfolio component

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "juanjosenavarro",
+      filename: "juanjosenavarro.js",
+      exposes: {
+        "./Porfolio": "./src/features/portfolio/portfolio.tsx",
+      },
+    }),
+  ],
   build: {
     cssCodeSplit: true,
     cssMinify: true,
