e10163a275089d643aeb8aaa79b8da77abac5acd
juan jose navarro perea <juanxo97master@gmail.com>
2024-07-13 11:39:35 +0200

    feat: add video and trace test e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,8 +28,8 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-    video: "retain-on-failure",
+    trace: "on",
+    video: "on",
     screenshot: "on",
   },
 
@@ -49,7 +49,6 @@ export default defineConfig({
     //   name: "webkit",
     //   use: { ...devices["Desktop Safari"] },
     // },
-
 
     /* Test against mobile viewports. */
     // {
